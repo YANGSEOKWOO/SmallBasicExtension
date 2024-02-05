64f6c6c7d0b1987298f9e331bcbc2137b01227db
YANGSEOKWOO <didtjrdn030@naver.com>
2024-02-05 18:02:17 +0900

    feat: Socket연결 Test

--- sb/out/extension.js
+++ sb/out/extension.js
@@ -1,4 +1,6 @@
 "use strict";
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -24,17 +26,44 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
-// 추가항목 Text라는 함수?
-// 그에 따른 GetCharacter
+const net = __importStar(require("net"));
 // document : VSCode에서 열려있는 텍스트 문서
 // position : 현재 커서의 위치
 // token : 작업이 취소되었는지 여부
 // context : 코드 완성이 제공되는 맥락
+const PORT = 50000;
+let socket;
+let link;
+let input;
+let output;
+let connect;
+// sendMessage : 텍스트 길이
+// cursorindex : 커서 위치
+// textArea : 전체 텍스트
 function activate(context) {
     console.log('Congratulations, your extension "sb" is now active!');
+    // accessServer1("localhost");
+    const testProvider = vscode.commands.registerCommand("sb.test", () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor) {
+            // 현재 열려있는 편집기의 문서 가져오기
+            const document = activeEditor.document;
+            // 문서의 텍스트 출력
+            // console.log("텍스트:", document.getText());
+            // 텍스트 길이 출력
+            // console.log("텍스트 길이:", document.getText().length);
+            // 커서 위치 가져오기
+            const cursorPosition = activeEditor.selection.active;
+            const cursorOffset = document.offsetAt(cursorPosition);
+            // 커서 위치 출력
+            // console.log("커서 위치:", cursorOffset);
+            serverConnect(document.getText().length.toString(), cursorOffset, document.getText());
+        }
+        else {
+            console.log("현재 열려있는 편집기가 없습니다.");
+        }
+    });
     const Completionprovider = vscode.languages.registerCompletionItemProvider("smallbasic", {
         provideCompletionItems(document, position, token, context) {
             // Text Object Completion
@@ -105,4 +134,73 @@ exports.activate = activate;
 // This method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+async function serverConnect(sendMessage, cursorindex, textArea) {
+    if (link === null) {
+        return;
+    }
+    await accessServer1("localhost");
+    // 커서 앞 텍스트 길이 보내기
+    console.log("커서 앞 텍스트 길이", sendMessage);
+    link.write(`${sendMessage} True`);
+    link.end();
+    await closingConnecting1();
+    // 커서 앞 텍스트 보내기
+    await accessServer1("localhost");
+    const frontMessage = textArea.substring(0, cursorindex);
+    console.log(" 커서 앞 텍스트 :", frontMessage);
+    link.write(frontMessage);
+    // link.write("For i = 1 ");
+    link.end();
+    await closingConnecting1();
+    // 커서 뒤 텍스트 보내기
+    await accessServer1("localhost");
+    const backMessage = textArea.substring(cursorindex, textArea.length);
+    console.log("커서 뒤 텍스트", backMessage);
+    link.write(backMessage);
+    // link.write("");
+    link.end();
+    await closingConnecting1();
+    // 구문 완성 결과를 문사열로 받기
+    await accessServer1("localhost");
+}
+async function accessServer1(host) {
+    try {
+        // 서버에 소켓 연결
+        await new Promise(resolve => {
+            link = new net.Socket();
+            if (link === null) {
+                return;
+            }
+            link.connect(PORT, host, () => {
+                console.log("Client connected");
+                resolve();
+            });
+            link.on("data", data => {
+                const decodedString = data.toString("utf-8");
+                console.log("서버에서 받은 데이터:", decodedString);
+            });
+            link.on("end", () => {
+                console.log("Client disconnected");
+            });
+        });
+    }
+    catch (error) {
+        // 연결이 거부될 경우 예외 처리
+        console.log("서버 연결 거부");
+        console.error(error.message);
+        connect = false;
+    }
+}
+function closingConnecting1() {
+    try {
+        if (link != null) {
+            console.log("연결종료");
+            link.end();
+            link = null;
+        }
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+}
 //# sourceMappingURL=extension.js.map
--- sb/src/extension.ts
+++ sb/src/extension.ts
@@ -1,15 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+
 // 추가항목 Text라는 함수?
 // 그에 따른 GetCharacter
-
+import { WebSocket } from "ws";
+import * as vscode from "vscode";
+import * as net from "net";
+import * as readline from "readline";
 // document : VSCode에서 열려있는 텍스트 문서
 // position : 현재 커서의 위치
 // token : 작업이 취소되었는지 여부
 // context : 코드 완성이 제공되는 맥락
+const PORT = 50000;
+let socket: WebSocket;
+
+let link: net.Socket | null;
+let input: readline.ReadLine;
+let output: NodeJS.WritableStream;
+let connect: boolean;
+// sendMessage : 텍스트 길이
+// cursorindex : 커서 위치
+// textArea : 전체 텍스트
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "sb" is now active!');
+  // accessServer1("localhost");
+
+  const testProvider = vscode.commands.registerCommand("sb.test", () => {
+    const activeEditor = vscode.window.activeTextEditor;
+
+    if (activeEditor) {
+      // 현재 열려있는 편집기의 문서 가져오기
+      const document = activeEditor.document;
+      // 문서의 텍스트 출력
+      // console.log("텍스트:", document.getText());
+
+      // 텍스트 길이 출력
+      // console.log("텍스트 길이:", document.getText().length);
+
+      // 커서 위치 가져오기
+      const cursorPosition = activeEditor.selection.active;
+      const cursorOffset = document.offsetAt(cursorPosition);
+      // 커서 위치 출력
+      // console.log("커서 위치:", cursorOffset);
+
+      serverConnect(
+        document.getText().length.toString(),
+        cursorOffset,
+        document.getText()
+      );
+    } else {
+      console.log("현재 열려있는 편집기가 없습니다.");
+    }
+  });
   const Completionprovider = vscode.languages.registerCompletionItemProvider(
     "smallbasic",
     {
@@ -139,3 +182,81 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function serverConnect(
+  sendMessage: string,
+  cursorindex: number,
+  textArea: string
+) {
+  if (link === null) {
+    return;
+  }
+  await accessServer1("localhost");
+  // 커서 앞 텍스트 길이 보내기
+  console.log("커서 앞 텍스트 길이", sendMessage);
+  link.write(`${sendMessage} True`);
+  link.end();
+  await closingConnecting1();
+
+  // 커서 앞 텍스트 보내기
+  await accessServer1("localhost");
+  const frontMessage = textArea.substring(0, cursorindex);
+  console.log(" 커서 앞 텍스트 :", frontMessage);
+  link.write(frontMessage);
+  // link.write("For i = 1 ");
+  link.end();
+  await closingConnecting1();
+
+  // 커서 뒤 텍스트 보내기
+  await accessServer1("localhost");
+  const backMessage = textArea.substring(cursorindex, textArea.length);
+  console.log("커서 뒤 텍스트", backMessage);
+  link.write(backMessage);
+  // link.write("");
+  link.end();
+  await closingConnecting1();
+
+  // 구문 완성 결과를 문사열로 받기
+  await accessServer1("localhost");
+}
+async function accessServer1(host: string): Promise<void> {
+  try {
+    // 서버에 소켓 연결
+    await new Promise<void>(resolve => {
+      link = new net.Socket();
+      if (link === null) {
+        return;
+      }
+      link.connect(PORT, host, () => {
+        console.log("Client connected");
+        resolve();
+      });
+
+      link.on("data", data => {
+        const decodedString = data.toString("utf-8");
+        console.log("서버에서 받은 데이터:", decodedString);
+      });
+
+      link.on("end", () => {
+        console.log("Client disconnected");
+      });
+    });
+  } catch (error: any) {
+    // 연결이 거부될 경우 예외 처리
+    console.log("서버 연결 거부");
+    console.error(error.message);
+    connect = false;
+  }
+}
+
+function closingConnecting1() {
+  try {
+    if (link != null) {
+      console.log("연결종료");
+      link.end();
+      link = null;
+    }
+  } catch (error: any) {
+    console.error(error.message);
+  }
+}
