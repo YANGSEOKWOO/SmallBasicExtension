f09e1c4a1316d4f6ef8418191572ab8a4e96d4c5
YANGSEOKWOO <didtjrdn030@naver.com>
2024-03-11 23:04:54 +0900

    feat: Sbsnippet 기능분리

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import { WebSocket } from "ws";
 import * as vscode from "vscode";
 import * as net from "net";
 import * as fs from "fs";
+import { SbSnippetGenerator } from "./sbSnippetGenerator";
 
 // document : VSCode에서 열려있는 텍스트 문서
 // position : 현재 커서의 위치
@@ -17,52 +18,15 @@ let link: net.Socket | null;
 
 let CompletionProvider: any;
 let stateNumber: number[] | null;
-let candidates: Item[];
+let candidates: CompletionItem[];
+let sbData: CompletionItem[];
 const path = require("path");
 
-type Item = {
+type CompletionItem = {
   key: string;
   value: number;
   sortText: string;
 };
-
-/**
- * state값을 통해 DB에 있는 state번호의 candidate를 가져와 key value형식으로 배열로 반환하는 함수
- * @param {string} string: state값
- * @returns key value 형태의 candidates배열 key: Completion name, value: frequency
- */
-function getCandidatesForStates(states: number[]) {
-  console.log("_dirname", __dirname);
-  const fileName = path.join(
-    __dirname,
-    "../src/smallbasic-syntax-completion-candidates-results.txt"
-  );
-  let fileContent = fs.readFileSync(fileName, "utf8");
-  const result: Item[] = [];
-
-  for (const state of states) {
-    const start = `State ${state}`;
-    const end = `State ${state + 1}`;
-    const startIdx = fileContent.indexOf(start);
-    const endIdx = fileContent.indexOf(end);
-    const Text = fileContent.substring(startIdx, endIdx);
-    console.log("Text :", Text);
-    const lines = Text.split("\n");
-    for (const line of lines) {
-      // State 문장 삭제
-      if (line[0] !== "[") {
-        continue;
-      }
-      const parts = line.split(":");
-      const key = parts[0].trim();
-      const value = parseInt(parts[1].trim());
-      const sortText = value.toString();
-      result.push({ key, value, sortText });
-    }
-  }
-  console.log("result", result);
-  return result;
-}
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "sb" is now active!');
@@ -75,12 +39,6 @@ export function activate(context: vscode.ExtensionContext) {
       const disposable = vscode.Disposable.from(CompletionProvider);
       disposable.dispose();
 
-      // 후보군에 Ranking 설정을 위한 sortText값 설정
-      candidates.sort((a, b) => b.value - a.value);
-      candidates.forEach((item, index) => {
-        item.sortText = (index + 1).toString().padStart(3, "0"); // 순위는 1부터 시작
-      });
-
       // 새로운 Completion 등록
       CompletionProvider = vscode.languages.registerCompletionItemProvider(
         "smallbasic",
@@ -92,87 +50,34 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.CompletionItem[] | vscode.CompletionList
           > {
             const CompletionItems: vscode.CompletionItem[] = [];
-            const targetStrings = ["[", "T", "N", "NT", "]", ","];
+            for (const { key, value, sortText } of sbData) {
+              const completion = new vscode.CompletionItem(key.trim());
+              console.log("completion 값:", completion);
+              const linePrefix = document
+                .lineAt(position)
+                .text.slice(0, position.character);
+              const lastIndex = linePrefix.length - 1;
+              const words = key.split(" ");
 
-            // 후보군을 completion에 등록하는 절차
-            for (const { key, value, sortText } of candidates) {
-              let completionWord = key;
+              // 각 단어에 TabStop 추가
+              const placeholders = words
+                .map((word, index) => `\${${index + 1}:${word}}`)
+                .join(" ");
 
-              // targetStrings 삭제 (ex. N, NT, [,])
-              completionWord = completionWord
-                .replace(/\[|\]/g, "") // 대괄호 삭제
-                .replace(/,/g, "") // 쉼표 삭제
-                .replace(/\s+\bT\b/g, " ") // 'T'를 공백으로 대체
-                .replace(/\bT\b/g, "") // 'T' 삭제
-                .replace(/\bNT\b/g, "") // 'NT' 삭제
-                .replace(/\s+/g, " ") // 여러 개의 공백을 1칸으로 변경
-                .replace(/\s+\./g, ".") // '.' 양옆의 공백 삭제
-                .replace(/\.\s+/g, ".") // '.' 양옆의 공백 삭제
-                .replace(/\bTO\b/g, " TO ");
-
-              const documentText = document.getText();
-              const variableFound = /\b([a-zA-Z_][a-zA-Z0-9_]*)\s*=/g;
-              let match;
-              let variableNames = [];
-              while ((match = variableFound.exec(documentText)) !== null) {
-                variableNames.push(match[1]);
-              }
-              console.log("variableName:", variableNames);
-              console.log(completionWord);
-              if (/\bFor\b/.test(completionWord)) {
-                const forLoopSnippet = new vscode.SnippetString(
-                  `For \${1|${variableNames.join(
-                    ","
-                  )}|} = \${2:lower} To \${3:upper} Step \${4:stepsize}\n` +
-                    `\t$0\n` +
-                    "EndFor" +
-                    "${TM_FILENAME/(.*)\\..+$/$1/}"
-                );
-                const completion = new vscode.CompletionItem(
-                  "For ID = Expr To Expr OptStep CRStmtCRs EndFor"
-                );
-                const completionDocs = new vscode.MarkdownString(
-                  "빈도수 : " + value
-                );
-                completion.documentation = completionDocs;
-                // completion.insertText = forLoopSnippet;
-                completion.sortText = sortText.toString();
-                CompletionItems.push(completion);
+              if (linePrefix[lastIndex] == " ") {
+                completion.insertText = new vscode.SnippetString(placeholders);
               } else {
-                const completion = new vscode.CompletionItem(
-                  completionWord.trim()
+                completion.insertText = new vscode.SnippetString(
+                  linePrefix + placeholders
                 );
-                const trimmedCompletionWord = completionWord.trim();
-                const snippetText = new vscode.SnippetString(
-                  `${trimmedCompletionWord.replace(
-                    /\bID\b/g,
-                    variableNames.length > 0
-                      ? "${1|" + variableNames.join(",") + "|}"
-                      : "${1:ID}"
-                  )}`
-                );
-                console.log("snippetText: ", snippetText);
-                completion.insertText = snippetText;
-
-                const linePrefix = document
-                  .lineAt(position)
-                  .text.slice(0, position.character);
-                // prefix 설정
-                completion.range = new vscode.Range(
-                  position.with(undefined, 0),
-                  position
-                );
-                completion.filterText = linePrefix;
-
-                // docs 설정 : Rank
-                const completionDocs = new vscode.MarkdownString(
-                  "빈도수 : " + value
-                );
-                completion.documentation = completionDocs;
-                completion.sortText = sortText.toString();
-                completion.insertText = linePrefix + completionWord;
-                CompletionItems.push(completion);
               }
+              completion.filterText = linePrefix;
+              completion.sortText = sortText;
+              const completionDocs = new vscode.MarkdownString(
+                "빈도수 : " + value
+              );
+              completion.documentation = completionDocs;
+              CompletionItems.push(completion);
             }
             return CompletionItems;
           },
@@ -194,7 +99,25 @@ export function activate(context: vscode.ExtensionContext) {
       // 커서 위치 가져오기
       const cursorPosition = activeEditor.selection.active;
       const cursorOffset = document.offsetAt(cursorPosition);
-      serverConnect(cursorOffset, document.getText());
+
+      const frontCursorTextLength = `${document
+        .getText()
+        .length.toString()} True`;
+      const frontCursorText = document.getText().substring(0, cursorOffset);
+      const backCursorText = document
+        .getText()
+        .substring(cursorOffset, document.getText().length);
+      const sbSnippetGenerator = new SbSnippetGenerator(
+        frontCursorTextLength,
+        frontCursorText,
+        backCursorText
+      );
+      sbSnippetGenerator.onDataReceived((data: any) => {
+        sbData = data;
+        vscode.commands.executeCommand("sb.subhotkey");
+      });
+      let dataE;
+      dataE = sbSnippetGenerator.getCompletionItems();
     } else {
       console.log("현재 열려있는 편집기가 없습니다.");
     }
@@ -205,99 +128,6 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(hotKeyProvider, completionCommand, codeTrigger);
-}
-
-/**
- * 서버와 통신하는 함수
- * @param {number}cursorindex 커서의 현재 위치
- * @param {string}textArea 열려있는 문서의 전체 텍스트
- *  */
-async function serverConnect(cursorindex: number, textArea: string) {
-  if (link === null) {
-    return;
-  }
-  accessServer1("localhost");
-  // 커서 앞 텍스트 길이 보내기
-  console.log("커서 앞 텍스트 길이", textArea.length.toString());
-  link.write(`${textArea.length.toString()} True`);
-  link.end();
-  closingConnecting1();
-
-  // 커서 앞 텍스트 보내기
-  accessServer1("localhost");
-  const frontMessage = textArea.substring(0, cursorindex);
-  console.log(" 커서 앞 텍스트 :", frontMessage);
-  link.write(frontMessage);
-  // link.write("For i = 1 ");
-  link.end();
-  closingConnecting1();
-
-  // 커서 뒤 텍스트 보내기
-  accessServer1("localhost");
-  const backMessage = textArea.substring(cursorindex, textArea.length);
-  console.log("커서 뒤 텍스트", backMessage);
-  link.write(backMessage);
-  // link.write("");
-  link.end();
-  closingConnecting1();
-
-  // 구문 완성 결과를 문사열로 받기
-  accessServer1("localhost");
-}
-
-/**
- * 서버에서 데이터를 받으면, state값으로 DB값을 읽어온 후 sb.completion명령어를 실행한다.
- * @param {string} 포트번호
- */
-function accessServer1(host: string) {
-  try {
-    // 서버에 소켓 연결
-    link = new net.Socket();
-    if (link === null) {
-      return;
-    }
-    link.connect(PORT, host, () => {
-      console.log("Client connected");
-    });
-
-    link.on("data", data => {
-      const decodedString = data.toString("utf-8");
-
-      if (decodedString === "SuccessfullyParsed") {
-        stateNumber = [0];
-      } else {
-        const extractedNumbers = decodedString.match(/\d+/g);
-        stateNumber = extractedNumbers ? extractedNumbers.map(Number) : [];
-        console.log("stateNumber", stateNumber);
-      }
-
-      candidates = getCandidatesForStates(stateNumber);
-      console.log("서버에서 받은 데이터:", decodedString);
-      vscode.commands.executeCommand("sb.subhotkey");
-    });
-
-    link.on("end", () => {
-      console.log("Client disconnected");
-    });
-  } catch (error: any) {
-    // 연결이 거부될 경우 예외 처리
-    console.log("서버 연결 거부");
-    console.error(error.message);
-  }
-}
-/**
- * 서버연결을 종료하는 함수
- */
-function closingConnecting1() {
-  try {
-    if (link != null) {
-      console.log("연결종료");
-      link.end();
-      link = null;
-    }
-  } catch (error: any) {
-    console.error(error.message);
-  }
 }
 
 // This method is called when your extension is deactivated
---
+++ src/sbSnippetGenerator.ts
@@ -0,0 +1,168 @@
+import * as vscode from "vscode";
+import { WebSocket } from "ws";
+import * as net from "net";
+import * as fs from "fs";
+//
+const PORT = 50000;
+let socket: WebSocket;
+
+let link: net.Socket | null;
+let stateNumber: number[] | null;
+let candidates: CompletionItem[];
+type CompletionItem = {
+  key: string;
+  value: number;
+  sortText: string;
+};
+const path = require("path");
+
+export class SbSnippetGenerator {
+  private readonly frontCursorTextLength: string;
+  private readonly frontCursorText: string;
+  private readonly backCursorText: string;
+
+  constructor(
+    frontCursorTextLength: string,
+    frontCursorText: string,
+    backCursorText: string
+  ) {
+    this.frontCursorTextLength = frontCursorTextLength;
+    this.frontCursorText = frontCursorText;
+    this.backCursorText = backCursorText;
+  }
+  public getCandidatesForStates(states: number[]) {
+    console.log("_dirname", __dirname);
+    const fileName = path.join(
+      __dirname,
+      "../src/smallbasic-syntax-completion-candidates-results.txt"
+    );
+    let fileContent = fs.readFileSync(fileName, "utf8");
+    const result: CompletionItem[] = [];
+
+    for (const state of states) {
+      const start = `State ${state}`;
+      const end = `State ${state + 1}`;
+      const startIdx = fileContent.indexOf(start);
+      const endIdx = fileContent.indexOf(end);
+      const Text = fileContent.substring(startIdx, endIdx);
+      console.log("Text :", Text);
+      const lines = Text.split("\n");
+      for (const line of lines) {
+        // State 문장 삭제
+        if (line[0] !== "[") {
+          continue;
+        }
+        const parts = line.split(":");
+        const key = parts[0].trim();
+        const value = parseInt(parts[1].trim());
+        const sortText = value.toString();
+        result.push({ key, value, sortText });
+      }
+    }
+    console.log("result", result);
+    return result;
+  }
+  public onDataReceived(callback: (data: any) => void) {
+    this.dataReceivedCallback = callback;
+  }
+  private dataReceivedCallback: ((data: any) => void) | null = null;
+  public accessServer1(host: string) {
+    try {
+      // 서버에 소켓 연결
+      link = new net.Socket();
+      if (link === null) {
+        return;
+      }
+      link.connect(PORT, host, () => {
+        console.log("Client connected");
+      });
+
+      link.on("data", data => {
+        const decodedString = data.toString("utf-8");
+
+        if (decodedString === "SuccessfullyParsed") {
+          stateNumber = [0];
+        } else {
+          const extractedNumbers = decodedString.match(/\d+/g);
+          stateNumber = extractedNumbers ? extractedNumbers.map(Number) : [];
+          console.log("stateNumber", stateNumber);
+        }
+        const a = 1;
+        candidates = this.getCandidatesForStates(stateNumber);
+        console.log("서버에서 받은 데이터:", decodedString);
+
+        // 후보군에 Ranking 설정을 위한 sortText값 설정
+        candidates.sort((a, b) => b.value - a.value);
+        candidates.forEach((item, index) => {
+          item.sortText = (index + 1).toString().padStart(3, "0"); // 순위는 1부터 시작
+        });
+
+        if (this.dataReceivedCallback) {
+          let completionItems: CompletionItem[] = [];
+          for (const { key, value, sortText } of candidates) {
+            let completionWord = key;
+            completionWord = completionWord
+              .replace(/\[|\]/g, "") // 대괄호 삭제
+              .replace(/,/g, "") // 쉼표 삭제
+              .replace(/\s+\bT\b/g, " ") // 'T'를 공백으로 대체
+              .replace(/\bT\b/g, "") // 'T' 삭제
+              .replace(/\bNT\b/g, "") // 'NT' 삭제
+              .replace(/\s+/g, " ") // 여러 개의 공백을 1칸으로 변경
+              .replace(/\s+\./g, ".") // '.' 양옆의 공백 삭제
+              .replace(/\.\s+/g, ".") // '.' 양옆의 공백 삭제
+              .replace(/\bTO\b/g, " TO ");
+            completionItems.push({
+              key: completionWord,
+              value: value,
+              sortText: sortText,
+            });
+          }
+          this.dataReceivedCallback(completionItems);
+        }
+      });
+
+      link.on("end", () => {
+        console.log("Client disconnected");
+      });
+    } catch (error: any) {
+      // 연결이 거부될 경우 예외 처리
+      console.log("서버 연결 거부");
+      console.error(error.message);
+    }
+  }
+  /**
+   * 서버연결을 종료하는 함수
+   */
+  public closingConnecting1() {
+    try {
+      if (link != null) {
+        console.log("연결종료");
+        link.end();
+        link = null;
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+  public getCompletionItems() {
+    if (link === null) {
+      return;
+    }
+    this.accessServer1("localhost");
+    link.write(`${this.frontCursorTextLength}`);
+    link.end();
+    this.closingConnecting1();
+
+    this.accessServer1("localhost");
+    link.write(`${this.frontCursorText}`);
+    link.end();
+    this.closingConnecting1();
+
+    this.accessServer1("localhost");
+    link.write(`${this.backCursorText}`);
+    link.end();
+    this.closingConnecting1();
+
+    this.accessServer1("localhost");
+  }
+}
